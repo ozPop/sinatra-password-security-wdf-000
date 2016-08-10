23f270e39034b04140dca831924bdb0cc5535d40
ozPop <ozziepopovas@gmail.com>
2016-08-10 11:11:11 -0400

    fix migration datatype, enable gitignore

--- db/migrate/20150916154312_create_users.rb
+++ db/migrate/20150916154312_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration
   def changes
     create_table :users do |col|
       col.string :username
-      col.password :password_digest
+      col.string :password_digest
     end
   end
 
